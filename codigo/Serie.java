e729f4420c361eda015e3964214fb77f10158dfc
pinheiro09igor <igor.santos.1202168@sga.pucminas.br>
2023-04-12 22:08:27 -0300

    Serie

--- codigo/Serie.java
+++ codigo/Serie.java
@@ -5,12 +5,16 @@ public class Serie {
     private int quantidadeEpisodios;
     private int audiencia;
 
-    public Serie(){
-        
+    public Serie(String[] qntGen, String nome, String idioma, int quantidadeEpisodios){
+        this.GENEROS = qntGen;
+        this.nome = nome;
+        this.idioma = idioma;
+        this.quantidadeEpisodios = quantidadeEpisodios;
+        this.audiencia = 0;
     }
 
     public void registrarAudiencia(){
-
+        audiencia++;
     }
 
 }
