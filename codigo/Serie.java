db7204b7d1cca937170613d24180546fe869e633
João Pedro de Oliveira Pauletti <jpedropauletti@gmail.com>
2023-04-12 22:14:50 -0300

    Javadoc

--- codigo/Serie.java
+++ codigo/Serie.java
@@ -1,10 +1,23 @@
+/**
+ * Classe Serie
+ */
 public class Serie {
+    /**
+     * Atributos da classe Serie
+     */
     private final String[] GENEROS;
     private String nome;
     private String idioma;
     private int quantidadeEpisodios;
     private int audiencia;
 
+    /**
+     * Construtor da classe Serie
+     * @param qntGen
+     * @param nome
+     * @param idioma
+     * @param quantidadeEpisodios
+     */
     public Serie(String[] qntGen, String nome, String idioma, int quantidadeEpisodios){
         this.GENEROS = qntGen;
         this.nome = nome;
@@ -13,8 +26,12 @@ public class Serie {
         this.audiencia = 0;
     }
 
+    /**
+     * Adiciona uma audiencia a serie
+     */
     public void registrarAudiencia(){
         audiencia++;
     }
 
 }
+
