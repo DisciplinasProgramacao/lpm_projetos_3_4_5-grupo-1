95d8465f8a216f50cdb3ad694b0815459e5498d8
luckaris <lutrinde@gmail.com>
2023-04-17 22:06:33 -0300

    Classe Série #2

--- codigo/Série.java
+++ codigo/Série.java
@@ -38,4 +38,69 @@ public class Série {
         this.audiência = 0;
     }
 
+    /**
+     * Adiciona uma audiência a série
+     */
+    public void registrarAudiência() {
+        audiência++;
+    }
+
+    /**
+     * Verifica se a sêrie possui o nome passado como parâmetro
+     * 
+     * @param nome a ser verificado
+     * @return TRUE se a série possui o nome, FALSE caso contrário
+     */
+    public boolean temNome(String nome) {
+        return this.nome.equals(nome);
+    }
+
+    /**
+     * Verifica se a sêrie possui o gênero passado como parâmetro
+     * 
+     * @param gênero a ser verificado
+     * @return TRUE se a série possui o gênero, FALSE caso contrário
+     */
+    public boolean possuiGênero(String gênero) {
+        return this.gênero.equals(gênero);
+    }
+
+    /**
+     * Verifica se a sêrie possui o idioma passado como parâmetro
+     * 
+     * @param idioma a ser verificado
+     * @return TRUE se a sêrie possui o idioma, FALSE caso contrario
+     */
+    public boolean possuiIdioma(String idioma) {
+        return this.idioma.equals(idioma);
+    }
+
+    /**
+     * Verifica se a sêrie possui a quantidade de episodios passada como parâmetro
+     * 
+     * @param qntEp a ser verificado
+     * @return TRUE se a sêrie possui a quantidade de episodios, FALSE caso
+     *         contrario
+     */
+    public boolean possuiEpisódios(int qntEp) {
+        return this.quantidadeEpisódios == qntEp;
+    }
+
+    /**
+     * Converte o objeto em uma String no formato: {IdSerie;Nome;DataDeLançamento}
+     */
+    @Override
+    public String toString() {
+        return nome + ";" + nome + ";" + "10";
+    }
+
+    /**
+     * Retorna o nome da série, usado em {@link Cliente#audiências()}
+     * 
+     * @return nome da série
+     */
+    public String getNome() {
+        return nome;
+    }
+
 }
